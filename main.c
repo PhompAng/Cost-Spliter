175085c00303da19a5cc916afae2a955b8e6a692
PhompAng <tingtong003tomy@gmail.com>
2015-03-18 17:13:18 +0700

    POI~

--- main.c
+++ main.c
@@ -2,6 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void draw_main();
+void add_expense();
+void add_remove_user();
+
+void add_expense() {
+    int choice;
+    printf("%s\n", "DONE");
+    draw_main();
+}
+
+void add_remove_user() {
+    int choice;
+    //clrscr();
+    printf("%s\n", "DONE");
+
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            draw_main();
+            break;
+        default:
+            add_remove_user();
+            break;
+    }
+
+}
+
+void draw_main() {
+    int choice;
+
+    //clrscr();
+    printf("%s\n", "1. Add/Remove User");
+    printf("%s\n", "2. Add Expense");
+    printf("%s\n", "0. exit");
+
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 0:
+            return;
+            break;
+        case 1:
+            add_remove_user();
+            break;
+        case 2:
+            add_expense();
+            break;
+        default:
+            draw_main();
+            break;
+    }
+
+}
+
 int main(void) {
 
     sqlite3 *db;
@@ -16,7 +71,9 @@ int main(void) {
 
         exit(0);
     }
-    sqlite3_close(db);
 
+    draw_main();
+    sqlite3_close(db);
+    printf("%s\n", "BYE");
     return 0;
 }
