c530278d6f762e9b3beddab3b0250f0d6f6915e3
PhompAng <tingtong003tomy@gmail.com>
2015-04-01 17:04:44 +0700

    Add Show result

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 void draw_main();
 void add_expense(char[], double, char[], char[]);
 void expense_manager();
+void show_result();
 void show_table(char[]);
 void add_remove_user();
 int callback(void *, int, char **, char **);
+int callback1(void *, int, char **, char **);
 sqlite3 *db;
 
 int callback(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -18,6 +20,14 @@ int callback(void *NotUsed, int argc, char **argv, char **azColName) {
         printf("%10.10s|", argv[i] ? argv[i] : "NULL");
     }
     printf("\n");
+    return 0;
+}
+int callback1(void *NotUsed, int argc, char **argv, char **azColName) {
+    NotUsed = 0;
+
+    for (int i = 0; i < argc; i++) {
+        printf("%10.10s|", argv[i] ? argv[i] : "NULL");
+    }
     return 0;
 }
 
@@ -140,6 +150,56 @@ void expense_manager() {
     }
 }
 
+
+void show_result() {
+    sqlite3_stmt *stmt;
+    int rc;
+    char *err_msg = 0;
+    char sql[999] = " ";
+    rc = sqlite3_prepare_v2(db, "SELECT * FROM paid_detail", -1, &stmt, 0);
+    if (rc != SQLITE_OK ) {
+        fprintf(stderr, "Failed to select data\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        exit(1);
+    }
+    int cols = sqlite3_column_count(stmt);
+    for (int i=1; i<cols; i++) {
+        sprintf(sql, "SELECT sum(%s) FROM paid_detail", sqlite3_column_name(stmt, i));
+        rc = sqlite3_exec(db, sql, callback1, 0, &err_msg);
+        if (rc != SQLITE_OK ) {
+            fprintf(stderr, "Failed to select data\n");
+            fprintf(stderr, "SQL error: %s\n", err_msg);
+
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+
+            exit(1);
+        }
+    }
+    printf("\n");
+    for (int i=1; i<cols; i++) {
+        sprintf(sql, "SELECT sum(%s) FROM spent_detail", sqlite3_column_name(stmt, i));
+        rc = sqlite3_exec(db, sql, callback1, 0, &err_msg);
+        if (rc != SQLITE_OK ) {
+            fprintf(stderr, "Failed to select data\n");
+            fprintf(stderr, "SQL error: %s\n", err_msg);
+
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+
+            exit(1);
+        }
+    }
+    printf("\n");
+
+    printf("\n=================================\n");
+
+}
+
 void show_table(char table[]) {
     int rc;
     char *err_msg = 0;
@@ -234,25 +294,28 @@ void add_remove_user() {
 void draw_main() {
     int choice;
 
-    system("clear");
     printf("%s\n", "1. Add/Remove User");
     printf("%s\n", "2. Add Expense");
+    printf("%s\n", "3. Show Result");
     printf("%s\n", "0. exit");
 
     printf("%s", "Enter choice: ");
     scanf("%d", &choice);
 
+    system("clear");
     switch(choice) {
         case 0:
             return;
             break;
         case 1:
-            system("clear");
             add_remove_user();
             break;
         case 2:
-            system("clear");
             expense_manager();
+            break;
+        case 3:
+            show_result();
+            draw_main();
             break;
         default:
             draw_main();
@@ -271,6 +334,7 @@ int main(void) {
         exit(0);
     }
 
+    system("clear");
     draw_main();
     sqlite3_close(db);
     printf("%s\n", "BYE");
